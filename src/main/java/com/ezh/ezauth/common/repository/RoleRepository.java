37c0acda656a341a0f0e9b80d693feffb4829161
Heeneth12 <heeneth123@gmail.com>
2026-02-03 23:08:04 +0530

    fix: changes in flyway added dummy data

--- src/main/java/com/ezh/ezauth/common/repository/RoleRepository.java
+++ src/main/java/com/ezh/ezauth/common/repository/RoleRepository.java
@@ -4,6 +4,11 @@ import com.ezh.ezauth.common.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    List<Role> findByTenantId(Long tenantId);
 }
